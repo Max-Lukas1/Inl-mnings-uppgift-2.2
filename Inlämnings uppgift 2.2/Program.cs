f528ef58d527cf41316cfadd1995b0ea5ac301ce
majo06002 <Max-Lukas@outlook.com>
2023-09-20 10:07:08 +0200

    Add project files.

---
+++ Inlämnings uppgift 2.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace inlämningsuppgift_2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade Alma?");
+            string Alma=Console.ReadLine();
+            Console.WriteLine("Hur långt hoppade Elin?");
+            string Elin = Console.ReadLine();
+            int Almas=int.Parse(Alma);
+            int Elins=int.Parse(Elin);
+            int Skillnad = Elins - Almas;
+            Console.WriteLine("Skillnaden är " + Skillnad + "cm");
+
+        }
+    }
+}
+
